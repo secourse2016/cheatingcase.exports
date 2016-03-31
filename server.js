82134425906bcb0d2d8e0954f4f9e4ee17837b30
Ahmed Omran <a.omran94@hotmail.com>
2016-04-01 01:29:30 +0200

    new Hazem Underconstruction 4

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express     =   require('express');
 var app         =   express();
 var fs          =   require('fs');
+var path        =   require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
