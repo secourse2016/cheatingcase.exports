eacdf2510d09c124bb7ea8bb5d1b69a4ff07ed70
sedki-abdelhakim <sedki.abdelhakim@gmail.com>
2016-04-01 18:16:34 +0200

    more house cleaning

---
+++ app.js
@@ -0,0 +1,14 @@
+var express     =   require('express');
+var app         =   express();
+var fs          =   require('fs');
+var path        =   require('path');
+
+app.use(express.static(path.join(__dirname, '')));
+
+app.get('/', function(req, res) {
+    fs.readFile(__dirname + '/index.html', 'utf8', function(err, text){
+      res.send(text);
+    });
+});
+
+module.exports = app
--- server.js
+++ server.js
@@ -1,15 +1,5 @@
-var express     =   require('express');
-var app         =   express();
-var fs          =   require('fs');
-var path        =   require('path');
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', function(req, res) {
-    fs.readFile(__dirname + '/index.html', 'utf8', function(err, text){
-      res.send(text);
-    });
-});
+var app =   require("./app.js");
 
 app.listen(3000, function(){
   console.log("App listening on port 3000 for http connections");
