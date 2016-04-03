94969bf382e6a1a1b5bea28d9967b256e1cb496a
MarkNader <marknader_2008@hotmail.com>
2016-04-03 12:57:16 +0200

    creating Core.js

---
+++ core.js
@@ -0,0 +1,20 @@
+var swissAir = angular.module('swissAir', ['ngRoute']);
+
+swissAir.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: 'js/controllers/mainController.js',
+      templateUrl: 'views/home.html'
+    })
+    .when('/flights', {
+      controller: 'js/controllers/flightsController.js',
+      templateUrl: 'views/flights.html'
+    })
+    .when('/pay', {
+      controller: 'js/controllers/payementController.js',
+      templateUrl: 'views/payement.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
