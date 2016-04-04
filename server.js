6a6c9cdcbaffee736d5718f52fc892f8edc12044
Tokyo <ahmed.tokyo1@gmail.com>
2016-04-04 21:52:24 +0200

    added seat no and class basic

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 var app =   require("./app.js");
 
-app.listen(80, function(){
-  console.log("App listening on port 80 for http connections");
+app.listen(3000, function(){
+  console.log("App listening on port 3000 for http connections");
 
 });
