46ec2308b605dbc9e559aa5d3a831f6fad647a81
Ahmed Omran <a.omran94@hotmail.com>
2016-04-18 22:50:34 +0200

    Just typo in AirportsSrv

--- public/services/AirportsSrv.js
+++ public/services/AirportsSrv.js
@@ -57,7 +57,7 @@ swissAir.factory('AirportsSrv', function ($http) {
 
 
          searchFlightsTwoWay: function(origin, destination, departingDate, returningDate, Class, otherAirlines){
-           var cls = (Class=='0')?'economy':'business');
+           var cls = (Class=='0')?'economy':'business';
            return $http.get('/api/flights/search/'+origin+'/'+destination+'/'+departingDate+'/'+returningDate+'/'+cls+''+'?oa='+otherAirlines, {
      				"headers" : { 'x-access-token' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzd2lzc0FpciIsImlhdCI6MTQ2MDYzMDIxMSwiZXhwIjoxNDkyMTY2MjE0LCJhdWQiOiJ3d3cuc3dpc3MtYWlyLm1lIiwic3ViIjoic3dpc3NBaXIgQ2xpZW50Iiwic3dpc3NBaXJVc2VyIjoic3dpc3NBaXJBbmd1bGFyIn0.GxAzq5SdDt8wB-2eqKBhaLAAHoCQ8Lw51yL2qRYbJvM'}
      		   });
