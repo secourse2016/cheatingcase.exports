f6ac4651bb2e5cb769230e2adeb04139bb4314cc
MarkNader <marknader_2008@hotmail.com>
2016-04-25 16:52:22 +0200

    Fixed initial position of button

--- public/directives/proceedToPay.js
+++ public/directives/proceedToPay.js
@@ -7,7 +7,7 @@ swissAir.directive('setClassWhenAtTop', ['$window', function($window) {
         {
             var topClass = attrs.setClassWhenAtTop,
                 topPadding = parseInt(attrs.paddingWhenAtTop, 10),
-                offsetTop = element.prop('offsetTop'); 
+                offsetTop = element.prop('offsetTop');
 
             $win.on('scroll', function (e) {
                 if ($window.pageYOffset + topPadding >= offsetTop) {
