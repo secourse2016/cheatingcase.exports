db555d962599d461af4d8399ea3f2e5ebce9d436
ahmed hazem <a.hazem1995@gmail.com>
2016-04-26 05:31:15 +0200

    home view modified

--- ionic/www/js/controllers.js
+++ ionic/www/js/controllers.js
@@ -1,6 +1,45 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, $ionicLoading, $ionicPopover) {
+   // .fromTemplate() method
+   var template = '<ion-popover-view>' + 
+                  '<ion-header-bar class="bar bar-assertive">' +
+                  '<h1 class = "title" style="text-align:center"><strong>Join us on</strong></h1>' +
+                  '</ion-header-bar>'+ '<ion-content>' +
+                  '<div class="list">'+
+                  '<div class="item item-icon-left">'+'<strong style="color:red"> facebook </strong>'+'<span class="badge badge-assertive">'+'<i class="ion-social-facebook"></i>'+'</span>'+'</div>'+
+                  '<div class="item item-icon-left">'+'<strong style="color:red"> twitter </strong>'+'<span class="badge badge-assertive">'+'<i class="ion-social-twitter"></i>'+'</span>'+'</div>'+
+                  '<div class="item item-icon-left">'+'<strong style="color:red"> instagram </strong>'+'<span class="badge badge-assertive">'+'<i class="ion-social-instagram"></i>'+'</span>'+'</div>'+
+                  '</div>' +
+                  '</ion-content>' + '</ion-popover-view>';
+
+   $scope.popover = $ionicPopover.fromTemplate(template, {
+      scope: $scope
+   });
+
+   $scope.openPopover = function($event) {
+      $scope.popover.show($event);
+   };
+
+   $scope.closePopover = function() {
+      $scope.popover.hide();
+   };
+
+   //Cleanup the popover when we're done with it!
+   $scope.$on('$destroy', function() {
+      $scope.popover.remove();
+   });
+
+   // Execute action on hide popover
+   $scope.$on('popover.hidden', function() {
+      // Execute action
+   });
+
+   // Execute action on remove popover
+   $scope.$on('popover.removed', function() {
+      // Execute action
+   });
+})
 
 .controller('MyCtrl', function($scope,$ionicSlideBoxDelegate,$location) {
    
@@ -8,6 +47,7 @@ angular.module('starter.controllers', [])
       $ionicSlideBoxDelegate.next();
    }
 });
+
 
 
 
