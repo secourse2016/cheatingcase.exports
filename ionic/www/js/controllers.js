cfc5251420b41b167c36f94ee983077e3be1aa2a
ahmed hazem <a.hazem1995@gmail.com>
2016-04-26 08:33:02 +0200

    controller.js modified

--- ionic/www/js/controllers.js
+++ ionic/www/js/controllers.js
@@ -40,10 +40,95 @@ angular.module('starter.controllers', [])
    });
 })
 
-.controller('MyCtrl', function($scope,$ionicSlideBoxDelegate,$location) {
+.controller('MyCtrl', function($scope,$ionicSlideBoxDelegate,$ionicLoading) {
    
     $scope.nextSlide = function() {
       $ionicSlideBoxDelegate.next();
+   };
+   $scope.showLoading = function() {
+      $ionicLoading.show({
+      	 //template: 'loading....',
+         delay:200,
+         duration: 1000,
+         hideOnStateChange: true,
+         noBackdrop: true,
+         templateUrl: '../templates/searchflights.html'
+      });
+   };
+
+   $scope.hideLoading = function(){
+      $ionicLoading.hide();
+   };
+})
+
+.controller('popupCtrl', function($scope, $ionicPopup) {
+
+   // When button is clicked, the popup will be shown...
+   $scope.showPopup1 = function() {
+      $scope.data = {}
+    
+      // Custom popup
+      var myPopup1 = $ionicPopup.show({
+         template: '<p style="font-family:Times New Roman">It was formed from a merger between Balair and Ad Astra Aero (To the Stars). For most of its 85 years, Swissair was one of the major international airlines.</p>',
+         title: 'Our Humble Beginnings',
+         subTitle: '<img src="./img/about1.jpg">',
+         scope: $scope,
+			
+         buttons: [
+             {
+               text: '<b>ok</b>',
+               type: 'button-assertive'
+            }
+         ]
+      });
+
+      myPopup1.then(function(res) {
+         console.log('Tapped!', res);
+      });    
+   },
+   $scope.showPopup2 = function() {
+      $scope.data = {}
+    
+      // Custom popup
+      var myPopup2 = $ionicPopup.show({
+         template: '<p style="font-family:Times New Roman">As Switzerland national airline, SWISS is committed to the highest standards of product and service quality. The airline flies some 16 million passengers every year to over 105 destinations all over the world.</p>',
+         title: 'For the people in the company',
+         subTitle: '<img src="./img/about2.jpg">',
+         scope: $scope,
+			
+         buttons: [
+             {
+               text: '<b>ok</b>',
+               type: 'button-assertive'
+            }
+         ]
+      });
+
+      myPopup2.then(function(res) {
+         console.log('Tapped!', res);
+      });    
+   },
+   $scope.showPopup3 = function() {
+      $scope.data = {}
+    
+      // Custom popup
+      var myPopup3 = $ionicPopup.show({
+         template: '<p style="font-family:Times New Roman">From its hub in Zurich and the regional airport of Geneva, Swiss International Air Lines flies to 105 destinations in 49 countries: 79 in Europe and 26 overseas. There are 94 aircraft in the airline fleet. SWISS is part of the Lufthansa Group and a member of the Star Alliance.</p>',
+         title: '16 million passengers,<br> 105 destinations.',
+         subTitle: '<img src="./img/about3.jpg">',
+         scope: $scope,
+			
+         buttons: [
+             {
+               text: '<b>ok</b>',
+               type: 'button-assertive'
+            }
+         ]
+      });
+
+      myPopup3.then(function(res) {
+         console.log('Tapped!', res);
+      });    
    }
 });
 
