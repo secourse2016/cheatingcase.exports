675492f299d25f9c10e91a4704262262f83d8824
sedki-abdelhakim <sedki.abdelhakim@gmail.com>
2016-04-28 00:22:22 +0200

    added a missing attribute (cost) to createBokking()

--- public/services/AirportsSrv.js
+++ public/services/AirportsSrv.js
@@ -128,7 +128,7 @@ swissAir.factory('AirportsSrv', function ($http) {
         "headers" : { 'x-access-token' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzd2lzc0FpciIsImlhdCI6MTQ2MDYzMDIxMSwiZXhwIjoxNDkyMTY2MjE0LCJhdWQiOiJ3d3cuc3dpc3MtYWlyLm1lIiwic3ViIjoic3dpc3NBaXIgQ2xpZW50Iiwic3dpc3NBaXJVc2VyIjoic3dpc3NBaXJBbmd1bGFyIn0.GxAzq5SdDt8wB-2eqKBhaLAAHoCQ8Lw51yL2qRYbJvM'}
       });
     },
-    createBooking: function(passengerDetails,outgoingFlightId,returnFlightId,paymentToken,localRequest,otherAirline){
+    createBooking: function(passengerDetails,cost,outgoingFlightId,returnFlightId,paymentToken,localRequest,otherAirline){
       var data = {
         "passengerDetails" :passengerDetails,
         "cost":cost,
