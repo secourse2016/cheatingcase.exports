476311dffa50df85b29f0153838fda2e29e24028
sedki-abdelhakim <sedki.abdelhakim@gmail.com>
2016-04-28 07:07:13 +0200

    introduced new function (viewBooking) that formulate http.get request with (refNum) to get the booking Info related to it

--- public/services/AirportsSrv.js
+++ public/services/AirportsSrv.js
@@ -115,6 +115,13 @@ swissAir.factory('AirportsSrv', function ($http) {
     setCost: function(value){
       this.cost=value;
     },
+    getbookingRefNum : function() {
+      return this.bookingRefNum;
+    },
+
+    setbookingRefNum: function(value) {
+      this.bookingRefNum = value;
+    },
 
 
 
@@ -141,6 +148,11 @@ swissAir.factory('AirportsSrv', function ($http) {
          path = '/booking' ;
       }
       return $http.post(path,data, {
+        "headers" : { 'x-access-token' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzd2lzc0FpciIsImlhdCI6MTQ2MDYzMDIxMSwiZXhwIjoxNDkyMTY2MjE0LCJhdWQiOiJ3d3cuc3dpc3MtYWlyLm1lIiwic3ViIjoic3dpc3NBaXIgQ2xpZW50Iiwic3dpc3NBaXJVc2VyIjoic3dpc3NBaXJBbmd1bGFyIn0.GxAzq5SdDt8wB-2eqKBhaLAAHoCQ8Lw51yL2qRYbJvM'}
+      });
+    },
+    viewBooking: function(refNum){
+      return $http.get('/viewbooking/'+refNum, {
         "headers" : { 'x-access-token' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzd2lzc0FpciIsImlhdCI6MTQ2MDYzMDIxMSwiZXhwIjoxNDkyMTY2MjE0LCJhdWQiOiJ3d3cuc3dpc3MtYWlyLm1lIiwic3ViIjoic3dpc3NBaXIgQ2xpZW50Iiwic3dpc3NBaXJVc2VyIjoic3dpc3NBaXJBbmd1bGFyIn0.GxAzq5SdDt8wB-2eqKBhaLAAHoCQ8Lw51yL2qRYbJvM'}
       });
     },
