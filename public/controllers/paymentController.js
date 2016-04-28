88bc7b5db7033221f115735a3dff0bb31a76e9b6
sedki-abdelhakim <sedki.abdelhakim@gmail.com>
2016-04-28 04:31:47 +0200

    completed the 4 forwarding cases ++ code refactored to be more readable

--- public/controllers/paymentController.js
+++ public/controllers/paymentController.js
@@ -1,72 +1,69 @@
- swissAir.controller('paymentController',function($scope,AirportsSrv,stripe){
+swissAir.controller('paymentController',function($scope,AirportsSrv,stripe){
 
-        //This backend is the remaining to handle : Sedki & Omran not Omran & Sedki :)
+  // retrieved Info About Outgoing Flight
+  $scope.outgoingFlightID= AirportsSrv.getOutgoingFlightID();
+  $scope.outgoingFlightAirline= AirportsSrv.getOutgoingFlightAirline();
 
-        // retrieved Info About Outgoing Flight
-        $scope.outgoingFlightID= AirportsSrv.getOutgoingFlightID();
-        $scope.outgoingFlightAirline= AirportsSrv.getOutgoingFlightAirline();
+  // retrieved Info About Return Flight
+  $scope.returnFlightID= AirportsSrv.getReturnFlightID();
+  $scope.returnFlightAirline= AirportsSrv.getReturnFlightAirline();
 
-        // retrieved Info About Return Flight
-        $scope.returnFlightID= AirportsSrv.getReturnFlightID();
-        $scope.returnFlightAirline= AirportsSrv.getReturnFlightAirline();
+  // retrieved Cost
+  $scope.cost= AirportsSrv.getCost();
 
-        // retrieved Cost
-        $scope.cost= AirportsSrv.getCost();
-
-        $scope.passengerDetails = AirportsSrv.getPassengerArray();
+  $scope.passengerDetails = AirportsSrv.getPassengerArray();
 
 
-        $scope.receipt_number= 0;
+  $scope.receipt_number= 0;
 
-        // Payment vars
-
-
-
-        $scope.book = function(){
-         stripe.card.createToken({
-            "number": $scope.cardnumber,
-            "cvc": $scope.cvCode,
-            "exp_month": $scope.cardExpMonth,
-            "exp_year": $scope.cardExpYear
-          }).then(function(paymentToken){
-            console.log(paymentToken);
-            if($scope.outgoingFlightAirline == $scope.returnFlightAirline || $scope.returnFlightAirline==undefined){
-               if($scope.outgoingFlightAirline=="Swiss Air"){
-                 AirportsSrv.createBooking($scope.passengerDetails,$scope.cost,$scope.outgoingFlightID,$scope.returnFlightID,paymentToken,true,$scope.outgoingFlightAirline).then(function(res){
-                   if(res.errorMessage!=null)
-                   $scope.refNum = res.refNum;
-                 });
-               }
-           else{
-             AirportsSrv.createBooking($scope.passengerDetails,$scope.cost,$scope.outgoingFlightID,$scope.returnFlightID,paymentToken,false,$scope.outgoingFlightAirline).then(function(res){
-               if(res.errorMessage!=null)
-               $scope.refNum = res.refNum;
-             });
-           }
-         }
-         else{
-           if(outgoingFlight.Airline!="Swiss Air" && returnFlight.Airline!= "Swiss Air"){
-             AirportsSrv.createBooking($scope.passengerDetails,$scope.cost,$scope.outgoingFlightID,null,paymentToken,false,$scope.outgoingFlightAirline);
-
-             AirportsSrv.createBooking($scope.passengerDetails,$scope.cost,$scope.returnFlightID,null,paymentToken,false,$scope.returnFlightAirline);
-           }
-           else{
-             if(outgoingFlight.Airline!="Swiss Air"){
-             AirportsSrv.createBooking($scope.passengerDetails,$scope.cost,$scope.outgoingFlightID,null,paymentToken,true,$scope.outgoingFlightAirline);
-             AirportsSrv.createBooking($scope.passengerDetails,$scope.cost,$scope.returnFlightID,null,paymentToken,false,$scope.returnFlightAirline);
-           }
-           AirportsSrv.createBooking($scope.passengerDetails,$scope.cost,$scope.outgoingFlightID,null,paymentToken,false,$scope.outgoingFlightAirline);
-           AirportsSrv.createBooking($scope.passengerDetails,$scope.cost,$scope.returnFlightID,null,paymentToken,true,$scope.returnFlightAirline);
-           }
-         }
-          });
-
-
-
-
-        };
-
-        $scope.$watch('receipt_number', function() {
-          $scope.bookingRefNumber = "JSW"+$scope.receipt_number;
+  $scope.book = function(){
+    stripe.card.createToken({
+      "number": $scope.cardnumber,
+      "cvc": $scope.cvCode,
+      "exp_month": $scope.cardExpMonth,
+      "exp_year": $scope.cardExpYear
+    }).then(function(paymentToken){
+      if($scope.outgoingFlightAirline == $scope.returnFlightAirline || $scope.returnFlightAirline==undefined){
+        AirportsSrv.createBooking($scope.passengerDetails,$scope.cost,$scope.outgoingFlightID,$scope.returnFlightID,paymentToken,$scope.outgoingFlightAirline).then(function(res){
+          console.log(res.errorMessage);
+          if(res.errorMessage==null){
+            console.log("same airline case true");
+            $scope.refNum = res.refNum;
+          }
+          else {
+            console.log("same airline case false");
+          }
         });
-     });
+
+      }
+      else{
+        AirportsSrv.createBooking($scope.passengerDetails,$scope.cost,$scope.outgoingFlightID,null,paymentToken,$scope.outgoingFlightAirline).then(function(resOutgoing){
+          console.log(resOutgoing.errorMessage);
+          if(resOutgoing.errorMessage==null){
+            console.log("different airlines outgoingFlight case true");
+            $scope.refNum = resOutgoing.refNum;
+          }
+          else {
+            console.log("different airlines outgoingFlight case false");
+
+          }
+          AirportsSrv.createBooking($scope.passengerDetails,$scope.cost,$scope.returnFlightID,null,paymentToken,$scope.returnFlightAirline).then(function(resReturn){
+            console.log(resReturn.errorMessage);
+            if(resReturn.errorMessage==null){
+              console.log("different airlines returnFlight case true");
+              $scope.refNum +="\n"+resReturn.refNum;
+            }
+            else {
+              console.log("different airlines returnFlight case false");
+            }
+          });
+        });
+
+      }
+    });
+  };
+
+  $scope.$watch('receipt_number', function() {
+    $scope.bookingRefNumber = "JSW"+$scope.receipt_number;
+  });
+});
