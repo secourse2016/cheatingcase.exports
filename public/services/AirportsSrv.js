848bc0750c3bf0d6492d3b1338da7c81543f7390
sedki-abdelhakim <sedki.abdelhakim@gmail.com>
2016-04-28 04:28:47 +0200

    code refactored to use single http.post instead ot two

--- public/services/AirportsSrv.js
+++ public/services/AirportsSrv.js
@@ -128,7 +128,7 @@ swissAir.factory('AirportsSrv', function ($http) {
         "headers" : { 'x-access-token' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzd2lzc0FpciIsImlhdCI6MTQ2MDYzMDIxMSwiZXhwIjoxNDkyMTY2MjE0LCJhdWQiOiJ3d3cuc3dpc3MtYWlyLm1lIiwic3ViIjoic3dpc3NBaXIgQ2xpZW50Iiwic3dpc3NBaXJVc2VyIjoic3dpc3NBaXJBbmd1bGFyIn0.GxAzq5SdDt8wB-2eqKBhaLAAHoCQ8Lw51yL2qRYbJvM'}
       });
     },
-    createBooking: function(passengerDetails,cost,outgoingFlightId,returnFlightId,paymentToken,localRequest,otherAirline){
+    createBooking: function(passengerDetails,cost,outgoingFlightId,returnFlightId,paymentToken,otherAirline){
       var data = {
         "passengerDetails" :passengerDetails,
         "cost":cost,
@@ -136,16 +136,13 @@ swissAir.factory('AirportsSrv', function ($http) {
         "returnFlightId":returnFlightId,
         "paymentToken" : paymentToken
       };
-      if(localRequest)
-      return $http.post('/booking',data, {
+      var path = '/bookingOthers' ;
+      if(otherAirline=="Swiss Air"){
+         path = '/booking' ;
+      }
+      return $http.post(path,data, {
         "headers" : { 'x-access-token' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzd2lzc0FpciIsImlhdCI6MTQ2MDYzMDIxMSwiZXhwIjoxNDkyMTY2MjE0LCJhdWQiOiJ3d3cuc3dpc3MtYWlyLm1lIiwic3ViIjoic3dpc3NBaXIgQ2xpZW50Iiwic3dpc3NBaXJVc2VyIjoic3dpc3NBaXJBbmd1bGFyIn0.GxAzq5SdDt8wB-2eqKBhaLAAHoCQ8Lw51yL2qRYbJvM'}
       });
-      else{
-        data.otherAirline = otherAirline;
-      return $http.post('/bookingOthers',data, {
-        "headers" : { 'x-access-token' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzd2lzc0FpciIsImlhdCI6MTQ2MDYzMDIxMSwiZXhwIjoxNDkyMTY2MjE0LCJhdWQiOiJ3d3cuc3dpc3MtYWlyLm1lIiwic3ViIjoic3dpc3NBaXIgQ2xpZW50Iiwic3dpc3NBaXJVc2VyIjoic3dpc3NBaXJBbmd1bGFyIn0.GxAzq5SdDt8wB-2eqKBhaLAAHoCQ8Lw51yL2qRYbJvM'}
-      });
-    }
     },
     getConcatFlightsTwoWay:function(origin, destination, departingDate, returningDate, Class, otherAirlines,cb){
       this.searchFlightsTwoWay(origin, destination, departingDate, returningDate, Class, otherAirlines)
