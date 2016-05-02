dd3d2fdce63d5641327bd1fd2500851fb8728b98
MarkNader <marknader_2008@hotmail.com>
2016-05-02 13:40:56 +0200

    Fixed script in index.html regarding angular messages

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -1,4 +1,4 @@
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','ngMessages'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
