d75f262d6a64485e92500bdc56e07f750eae240b
sedki-abdelhakim <sedki.abdelhakim@gmail.com>
2016-05-04 16:34:37 +0200

    added contactUS() function over the contactusController scope

--- public/controllers/contactusController.js
+++ public/controllers/contactusController.js
@@ -1,5 +1,6 @@
 swissAir.controller('contactusController',function($scope,AirportsSrv){
-    
+    $scope.contactUs = function(){
 AirportsSrv.contactus($scope.name,$scope.email,$scope.phone,$scope.message);
+    }
 
 });
