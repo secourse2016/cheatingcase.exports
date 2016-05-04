5ef63563f3807c190d19a6cbd1f961ae1e8deca4
sedki-abdelhakim <sedki.abdelhakim@gmail.com>
2016-05-04 15:54:23 +0200

    added contactus function to forward a post request containing client info to our server

--- public/services/AirportsSrv.js
+++ public/services/AirportsSrv.js
@@ -162,6 +162,19 @@ swissAir.factory('AirportsSrv', function ($http) {
       return $http.post(path,data, {
         "headers" : { 'x-access-token' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzd2lzc0FpciIsImlhdCI6MTQ2MDYzMDIxMSwiZXhwIjoxNDkyMTY2MjE0LCJhdWQiOiJ3d3cuc3dpc3MtYWlyLm1lIiwic3ViIjoic3dpc3NBaXIgQ2xpZW50Iiwic3dpc3NBaXJVc2VyIjoic3dpc3NBaXJBbmd1bGFyIn0.GxAzq5SdDt8wB-2eqKBhaLAAHoCQ8Lw51yL2qRYbJvM'}
       });
+    }, 
+    contactus: function(name,email,phone ,message){
+      var data = {
+        "name": name,
+        "email": email,
+        "phone": phone,
+        "message": message,
+      };
+      var path = '/contactus' ;
+      
+      return $http.post(path,data, {
+        "headers" : { 'x-access-token' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzd2lzc0FpciIsImlhdCI6MTQ2MDYzMDIxMSwiZXhwIjoxNDkyMTY2MjE0LCJhdWQiOiJ3d3cuc3dpc3MtYWlyLm1lIiwic3ViIjoic3dpc3NBaXIgQ2xpZW50Iiwic3dpc3NBaXJVc2VyIjoic3dpc3NBaXJBbmd1bGFyIn0.GxAzq5SdDt8wB-2eqKBhaLAAHoCQ8Lw51yL2qRYbJvM'}
+      });
     },
     viewBooking: function(refNum){
       return $http.get('/viewbooking/'+refNum, {
